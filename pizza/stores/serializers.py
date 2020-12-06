bb703653c9e3eed8d1dc4903c46fd76024085a0b
abhigyan <abhigyan709@gmail.com>
2020-12-06 13:34:41 +0530

    Serializers Made

---
+++ pizza/stores/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Pizzeria
+
+
+class PizzeriaListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pizzeria
+        fields = [
+            "id",
+            "pizzeria_name",
+            "city",
+            "zip_code",
+        ]
--- pizza/stores/views.py
+++ pizza/stores/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PizzeriaListSerializer
 
-# Create your views here.
