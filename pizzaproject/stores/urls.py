37ba18bfd14d364d305c936af323d51d57e985ff
abhigyan <abhigyan709@gmail.com>
2020-12-10 16:52:39 +0530

    "All Files Deleted"

--- pizzaproject/stores/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PizzeriaListAPIView.as_view(), name="pizzeria_list"),
-    path('<int:id>/', views.PizzeriaRetrieveAPIView.as_view(), name="pizzeria_detail"),
-
-]
