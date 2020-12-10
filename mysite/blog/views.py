0887cfe0a41c22a144390fe186d17aa2f3bc6888
abhigyan <abhigyan709@gmail.com>
2020-12-11 02:02:10 +0530

    "Some advanced backend features of blog post"

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
-def post_list(request):
+"""def post_list(request):
     posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts})"""
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 post in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -18,3 +39,32 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_url(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you read "\
+                      f"{post.title}"
+            message
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -114,6 +114,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'abhigyan709@gmail.com'
+EMAIL_HOST_PASSWORD = 'abhigyan7761928879'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
