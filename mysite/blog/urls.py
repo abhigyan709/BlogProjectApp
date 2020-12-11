925c38179f53329e5ec27aa3b3003436f4f68b96
abhigyan <abhigyan709@gmail.com>
2020-12-12 01:36:51 +0530

    "Some advanced backend features of blog post"

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    #path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:post_id>/share/', views.post_share, name='post_share')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,17 +5,16 @@ from django.core.paginator import Paginator, EmptyPage,\
 from django.views.generic import ListView, DetailView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-"""def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})"""
-
-
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tag__in=[tag])
     paginator = Paginator(object_list, 3)  # 3 post in each page
     page = request.GET.get('page')
     try:
@@ -27,7 +26,8 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
