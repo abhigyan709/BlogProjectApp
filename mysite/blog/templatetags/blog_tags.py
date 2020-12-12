02645c9fcde864e10470c7079ed9fe12fd4bef3e
abhigyan <abhigyan709@gmail.com>
2020-12-13 01:55:51 +0530

    "Added markdown system to the post"

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -21,3 +23,8 @@ def get_most_commented_posts(count=5):
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
