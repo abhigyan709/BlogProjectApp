97a89e067369645f4cb3dac4313dc5b9db19afca
abhigyan <abhigyan709@gmail.com>
2020-12-13 18:10:56 +0530

    "Search Functionality successfully added"

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('feed/', LatestPostFeed(), name='post_feed'),
+    path('search', views.post_search, name='post_search'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,12 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView, DetailView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -63,8 +65,6 @@ def post_detail(request, year, month, day, post):
     similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
                         .order_by('-same_tags', '-publish')[:4]
 
-
-
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
@@ -106,3 +106,22 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(request,'blog/post/search.html', {'form': form,
+                                                    'query': query,
+                                                    'results': results})
