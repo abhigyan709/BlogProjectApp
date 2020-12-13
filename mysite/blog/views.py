eeb00951f483f6e7f479935b22173bc6eeb72c97
abhigyan <abhigyan709@gmail.com>
2020-12-13 19:25:33 +0530

    "Blog completed with all basic features"

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-
+from django.contrib.postgres.search import TrigramSimilarity
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -116,12 +116,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
-    return render(request,'blog/post/search.html', {'form': form,
-                                                    'query': query,
-                                                    'results': results})
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
